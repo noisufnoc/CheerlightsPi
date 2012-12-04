33420df3020ddacfaff8093df41a137edb645029
Mike Walker <noisufnoc@gmail.com>
2012-12-04 09:43:45 -0800

    updated rpi code to talk to arduino

--- raspi/cheerlights.py
+++ raspi/cheerlights.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python
+import serial
 import time
 import requests
+
+# Setup serial connection to arduino
+
+DEVICE = '/dev/ttyACM0'
+BAUD = 9600
+arduino = serial.Serial(DEVICE, BAUD)
 
 # Read the thingspeak feed to get the current color
 
 while True:
-	cheerlights_feed = requests.get('http://api.thingspeak.com/channels/1417/field/1/last.json').json
-
-	# ToDo: Update the LED using GPIO
-	print cheerlights_feed['field1']
-
-	# ToDo: Find thinkspeek update interval to not DoS them every 5 seconds.
-	time.sleep(5)
+        cheerlights_feed = requests.get('http://api.thingspeak.com/channels/1417/field/1/last.json').json
+        arduino.write(cheerlights_feed['field1'])
+        print cheerlights_feed['field1']
+        time.sleep(16)
